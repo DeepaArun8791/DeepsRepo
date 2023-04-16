2b39d087c7208edb704c9fc10d78e93985bdffd9
DeepaArun8791 <119514063+DeepaArun8791@users.noreply.github.com>
2023-04-16 15:46:51 +0530

    Commented test libraries.

--- src/test/java/guru/springframework/repositories/ProductRepositoryTest.java
+++ src/test/java/guru/springframework/repositories/ProductRepositoryTest.java
@@ -5,15 +5,15 @@ import guru.springframework.domain.Product;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = {RepositoryConfiguration.class})
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest(classes = {RepositoryConfiguration.class})
 public class ProductRepositoryTest {
 
     private ProductRepository productRepository;
