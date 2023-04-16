63907c58169d602802462726870c9564ad684c84
DeepaArun8791 <119514063+DeepaArun8791@users.noreply.github.com>
2023-04-16 15:42:31 +0530

    Commented test libraries.

--- src/test/java/guru/springframework/SpringBootWebApplicationTests.java
+++ src/test/java/guru/springframework/SpringBootWebApplicationTests.java
@@ -2,13 +2,13 @@ package guru.springframework;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = SpringBootWebApplication.class)
-@WebAppConfiguration
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest(classes = SpringBootWebApplication.class)
+//@WebAppConfiguration
 public class SpringBootWebApplicationTests {
 
 	@Test
